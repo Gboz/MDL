c97b08502cf4b437113dd15dc0bad795cba7edb6
gboz <gbolesta21@gmail.com>
2017-12-14 12:34:01 +0100

    Add new Activity

--- app/src/main/java/pl/delevel/mdl/mdl/MainActivity.java
+++ app/src/main/java/pl/delevel/mdl/mdl/MainActivity.java
@@ -1,7 +1,10 @@
 package pl.delevel.mdl.mdl;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button enterApp = (Button) findViewById(R.id.enterAppActivityBtn);
+        enterApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent enterIntent = new Intent(getApplicationContext(), MenuActivity.class);
+                startActivity(enterIntent);
+            }
+        });
     }
 }
